dbddae90a01180cbd440b7cd6f57c06252d1c790
DharaniMurugan01 <2k21dhara@gmail.com>
2025-03-27 06:16:12 +0530

    commited again

--- Demowebshop/src/test/java/com/base/Basetest.java
+++ Demowebshop/src/test/java/com/base/Basetest.java
@@ -1,20 +1,27 @@
 package com.base;
+
 import java.time.Duration;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
- public class Basetest {
-	protected WebDriver driver;
+
+public class Basetest {
+    protected WebDriver driver;
+
     @BeforeMethod
     public void setUp() {
-        driver=new EdgeDriver();
+        System.setProperty("webdriver.edge.driver", "C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\134.0.3124.85\\msedgedriver.exe");        
+        driver = new EdgeDriver();
         driver.get("https://demowebshop.tricentis.com/");
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
     }
+
     @AfterMethod
     public void tearDown() {
-      driver.quit();
+        if (driver != null) {
+            driver.quit();
+        }
     }
 }
