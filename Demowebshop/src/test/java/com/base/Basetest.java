bc4da25e48fb8cde6cd3000dd56ecdf48a638d13
DharaniMurugan01 <2k21dhara@gmail.com>
2025-03-27 06:22:14 +0530

    commited for chrome

--- Demowebshop/src/test/java/com/base/Basetest.java
+++ Demowebshop/src/test/java/com/base/Basetest.java
@@ -1,27 +1,20 @@
 package com.base;
-
 import java.time.Duration;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-
-public class Basetest {
-    protected WebDriver driver;
-
+ public class Basetest {
+	protected WebDriver driver;
     @BeforeMethod
     public void setUp() {
-        System.setProperty("webdriver.edge.driver", "C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\134.0.3124.85\\msedgedriver.exe");        
-        driver = new EdgeDriver();
+        driver=new ChromeDriver();
         driver.get("https://demowebshop.tricentis.com/");
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
     }
-
     @AfterMethod
     public void tearDown() {
-        if (driver != null) {
-            driver.quit();
-        }
+      driver.quit();
     }
 }
